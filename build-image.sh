30cd54e8fcc06b29e2d6b4ccc12035a0ffdff898
Wirawan Purwanto <wpurwant@odu.edu>
2016-07-05 18:19:43 -0400

    Fixes to allow this script to run in a docker container.

--- build-image.sh
+++ build-image.sh
@@ -13,6 +13,10 @@ packages="debootstrap dchroot apparmor"
 which docker || packages="$packages docker.io"
 apt-get install -y $packages
 
+if [ -d $chroot_dir ]; then
+    rm -rf $chroot_dir
+fi
+
 ### install a minbase system with debootstrap
 export DEBIAN_FRONTEND=noninteractive
 debootstrap --variant=minbase --arch=$arch $suite $chroot_dir $apt_mirror
@@ -29,6 +33,18 @@ EOF
 ### install ubuntu-minimal
 cp /etc/resolv.conf $chroot_dir/etc/resolv.conf
 mount -o bind /proc $chroot_dir/proc
+mount -t sysfs none $chroot_dir/sys
+mkdir -p $chroot_dir/dev/pts
+mount -t devpts none $chroot_dir/dev/pts
+
+if [ "x$INSTALL_IN_DOCKER" = xyes ]; then
+    # Divert away initctl temporarily to allow proper installation and
+    # "config" by apt-get
+    # ref: https://github.com/docker/docker/issues/1024#issuecomment-20018600
+    chroot $chroot_dir dpkg-divert --local --rename --add /sbin/initctl
+    chroot $chroot_dir ln -sf /bin/true /sbin/initctl
+fi
+
 chroot $chroot_dir apt-get update
 chroot $chroot_dir apt-get -y upgrade
 chroot $chroot_dir apt-get -y install ubuntu-minimal
@@ -38,6 +54,16 @@ chroot $chroot_dir apt-get autoclean
 chroot $chroot_dir apt-get clean
 chroot $chroot_dir apt-get autoremove
 rm $chroot_dir/etc/resolv.conf
+
+if [ "x$INSTALL_IN_DOCKER" = xyes ]; then
+    # Undo diversion
+    chroot $chroot_dir rm /sbin/initctl
+    chroot $chroot_dir dpkg-divert --rename --remove /sbin/initctl
+fi
+
+### unmount other auxiliary mount points, first
+umount $chroot_dir/dev/pts
+umount $chroot_dir/sys
 
 ### kill any processes that are running on chroot
 chroot_pids=$(for p in /proc/*/root; do ls -l $p; done | grep $chroot_dir | cut -d'/' -f3)
